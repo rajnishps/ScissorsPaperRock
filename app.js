37eb3eddb6bd88d41f5735954e8389cbe69c001b
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2022-08-25 08:39:18 +0530

    comparison added

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
-const playerScore = document.querySelector("#player-score");
 const theResult = document.querySelector("#result");
+const compScore = document.querySelector("#comp-score");
+const playerScore = document.querySelector("#player-score");
+const compHand = document.querySelector("#hand");
 const personOption = document.querySelectorAll(".rpsButton");
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+  const getComputerChoice = (playerChoice) => {
+    const compOptions = ["Rock", "Paper", "Scissors"];
+    let computerChoice = compOptions[Math.floor(Math.random() * 3)];
+    compare(playerChoice, computerChoice);
+  };
 
-const getComputerChoice = (playerChoice) => {
-  const compOptions = ["Rock", "Paper", "Scissors"];
-  let computerChoice = compOptions[Math.floor(Math.random() * 3)];
-  compare(playerChoice ,computerChoice);
-};
-
-const playerChoice = (computerChoice) => {
-  personOption.forEach((option) => {
-    option.addEventListener("click", () => {
-      getComputerChoice(option.value)
+  const playerChoice = (computerChoice) => {
+    personOption.forEach((option) => {
+      option.addEventListener("click", () => {
+        getComputerChoice(option.value);
+      });
     });
-  });
-};
+  };
 
-const compare = (playerChoice,computerChoice) => {
-  console.log(playerChoice,computerChoice)
+  const compare = (playerChoice, computerChoice) => {
+    compHand.textContent = `Computer's Choice ${computerChoice}`;
+    if (playerChoice === "Rock") {
+      if (computerChoice === "Paper") {
+        cScore += 1;
+        theResult.textContent = "Computer Wins";
+        compScore.textContent = `Computer's Score: ${cScore}`;
+      } else if (computerChoice === "Scissors") {
+        pScore += 1;
+        theResult.textContent = "Player Wins";
+        playerScore.textContent = `Player's Score: ${cScore}`;
+      } else {
+        theResult.textContent = "Draw";
+      }
+    } else if (playerChoice === "Paper") {
+      if (computerChoice === "Scissors") {
+        cScore += 1;
+        theResult.textContent = "Computer Wins";
+        compScore.textContent = `Computer's Score: ${cScore}`;
+      } else if (computerChoice === "Rock") {
+        pScore += 1;
+        theResult.textContent = "Player Wins";
+        playerScore.textContent = `Player's Score: ${cScore}`;
+      } else {
+        theResult.textContent = "Draw";
+      }
+    } else if (playerChoice === "Scissors") {
+      if (computerChoice === "Rock") {
+        cScore += 1;
+        theResult.textContent = "Computer Wins";
+        compScore.textContent = `Computer's Score: ${cScore}`;
+      } else if (computerChoice === "Paper") {
+        pScore += 1;
+        theResult.textContent = "Player Wins";
+        playerScore.textContent = `Player's Score: ${cScore}`;
+      } else {
+        theResult.textContent = "Draw";
+      }
+    }
+  };
+  playerChoice();
 };
-
-playerChoice()
+game();
